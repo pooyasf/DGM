3949466b20103d96015c4b607a7c8107038bc899
Pooya <pooyas1373@gmail.com>
2020-11-29 17:54:41 +0330

    files

---
+++ Heat/heat.py
@@ -0,0 +1,55 @@
+from libs import *
+
+class Heat():
+    
+    def __init__(self , net):
+        
+        ## for accept reject purpose!
+        ## free boundry problems
+        self.net = net
+
+    def sample(self , ts = 0 , te = np.pi , xs = 0 , xe = np.pi , size = 2**8 ):
+         
+         x = torch.cat(( torch.rand( [size,1] )*te  , torch.rand( [size,1] )*xe  ) , dim = 1 ).cuda()
+         x_initial = torch.cat(( torch.zeros(size, 1)  , torch.rand( [size,1] )*xe  ) , dim = 1 ).cuda()
+            
+         x_boundry_start = torch.cat(( torch.rand( [size,1] )*te , torch.zeros(size, 1)  ) , dim = 1 ).cuda()
+         x_boundry_end = torch.cat(( torch.rand( [size,1] )*te  , torch.zeros(size, 1)  ) , dim = 1 ).cuda()
+         
+         return x , x_initial , x_boundry_start , x_boundry_end
+    
+    
+    def criterion(self , x , x_initial , x_boundry_0 , x_boundry_pi):
+        
+        d = torch.autograd.grad(self.net(x), x , grad_outputs=torch.ones_like(self.net(x)) ,\
+                                create_graph=True , retain_graph = True)
+        dt = d[0][:,0].reshape(-1,1)
+        dx = d[0][:,1].reshape(-1,1)
+        # du/dxdx
+        dxx = torch.autograd.grad(dx, x , grad_outputs=torch.ones_like(dx) ,\
+                                  retain_graph = True)[0][:,1].reshape(-1,1)
+        
+        # Domain 
+        DO = ( dt - dxx )**2
+        # Terminal Condition
+        IC = ( (torch.sin(x_initial[:,1].reshape(-1,1) )) - self.net(x_initial) )**2
+        BD_0  = ( self.net(x_boundry_0)  - torch.zeros(len(x_boundry_0), 1).cuda() )**2
+        BD_pi = ( self.net(x_boundry_pi) - torch.zeros(len(x_boundry_pi), 1).cuda() )**2
+        
+        return  DO , IC , BD_0 , BD_pi
+
+
+    def calculateLoss(self , size = 2**8 , train = True):
+        
+        x , x_initial , x_boundry_0 , x_boundry_pi = self.sample(size)
+        x = Variable( x , requires_grad=True)
+        DO , IC , BD_0  , BD_pi = self.criterion( x , x_initial , x_boundry_0 , x_boundry_pi )
+        
+        if train == True:
+            return  torch.mean(DO + IC + BD_0 + BD_pi) , torch.mean( DO ) , torch.mean( IC ) , torch.mean( BD_0 + BD_pi )  
+        else:
+            return  DO , IC , BD_0 , BD_pi
+        
+    
+    def exact_solution(self , t , x ):
+        return np.sin(x)*np.exp(-1*t)
---
+++ Heat/train.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 09:48:58 2020
+
+@author: Pooya
+"""
+
+from libs import *
+from hook import *
+
+
+class Train():
+
+    
+    def __init__(self , net , heatequation , BATCH_SIZE , debug = False):
+        
+        
+        self.history_mean_hooks = []
+        
+        self.history_tl = []
+        self.history_dl = []
+        self.history_il = []
+        self.history_bl = []
+              
+        self.BATCH_SIZE = BATCH_SIZE
+        self.net = net
+        self.model = heatequation
+        
+        self.debug = debug
+        
+        if self.debug == True:
+            self.hooks = {}
+            
+            self.get_all_layers(self.net)
+        
+    
+    def train(self , epoch , lr ):
+                
+        
+        optimizer = optim.Adam(self.net.parameters(), lr)
+        
+        loss_avg = 0
+        
+        for e in range(epoch):
+            
+            
+            optimizer.zero_grad()
+            loss , _ , _ , _ = self.model.calculateLoss( self.BATCH_SIZE )
+            loss_avg = loss_avg + loss.item()
+            loss.backward()
+            optimizer.step()
+            
+            if e % 50 == 49:
+                
+                loss = loss_avg/50
+                print("Epoch {} - lr {} -  loss: {}".format(e , lr , loss ))
+                loss_avg = 0
+
+                #history_validation_de.append( validate_DE()[0] )
+                
+                ## report detailed loss ##
+
+                tl , dl , il , bl = self.model.calculateLoss( 2**10 )
+                
+                self.history_tl.append( tl )
+                self.history_dl.append( dl )
+                self.history_il.append( il )
+                self.history_bl.append( bl )
+                
+                
+                if self.debug == True:
+                    mean = []
+                    for l in self.hooks:
+                        mean.append(torch.mean( self.hooks[l] ).item())
+                    
+                    self.history_mean_hooks.append( mean )
+
+
+            
+    def plot_report(self):
+        
+        
+        fig, ax = plt.subplots(4, 1 ,constrained_layout=True)
+        ax[0].plot( np.log(self.history_tl) , '-b', label='total')
+        
+        ax[0].set_title('total')
+        fig.suptitle('Training Loss', fontsize=10)
+        
+        ax[1].plot( np.log(self.history_dl) )
+        ax[1].set_title('diff operator')
+        
+        
+        ax[2].plot( np.log(self.history_il) )
+        ax[2].set_title('initial condition')
+        
+        
+        ax[3].plot( np.log(self.history_bl) )
+        ax[3].set_title('boundry condition')
+        
+
+    
+    def hook_fn(self, m, i, o):
+              self.hooks[m] = o.detach()
+            
+    def get_all_layers(self, net):
+      for name, layer in net._modules.items():
+          if isinstance(layer, nn.ModuleList):
+              for n , l in layer.named_children():
+                l.register_forward_hook(self.hook_fn)
+          else:
+              # it's a non sequential. Register a hook
+              layer.register_forward_hook(self.hook_fn)
+            
+    
+    
+    
+    def plot_activation_mean(self):
+        
+        if self.debug == False:
+            print( 'error: debug is off , turn it on and train again ' )
+        else:
+            history = np.array(self.history_mean_hooks)
+            jet= plt.get_cmap('jet')
+            colors = iter(jet(np.linspace(0,1,10)))
+            fig, ax = plt.subplots()
+            for i in range(history.shape[1]):
+                ax.plot(history[:,i], '--r', label= i , color=next(colors) )
+            
+            fig.suptitle('Layers activation mean value', fontsize=10)
+            leg = ax.legend();
+
+    
