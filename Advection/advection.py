b8071e4bef06158b5e0e7a67bb66b682daa73ea9
Pooya <pooyas1373@gmail.com>
2021-01-23 10:07:42 +0330

    Advection added

---
+++ Advection/advection.py
@@ -0,0 +1,46 @@
+from libs import *
+
+class Advection():
+    
+    def __init__(self , net):
+        
+        self.net = net
+
+    def sample(self , xs = 0 , xe = 1 , size = 2**8 ):
+         
+         x = (torch.rand( [size,1] )*xe  ).cuda()
+            
+         x_initial = torch.zeros(size, 1).cuda() 
+         
+         
+         return x , x_initial
+    
+    
+    def criterion(self , x  , x_initial ):
+        
+        d = torch.autograd.grad(self.net(x), x , grad_outputs=torch.ones_like(self.net(x)) ,\
+                                create_graph=True , retain_graph = True)
+        
+        dx = d[0].reshape(-1,1)
+        
+        # Domain 
+        DO = ( dx - 2*np.pi*torch.cos(2*np.pi*x)*torch.cos(4*np.pi*x) + 4*np.pi*torch.sin(4*np.pi*x)*torch.sin(2*np.pi*x) )**2
+        IC  = ( self.net(x_initial)  - torch.ones(len(x_initial), 1).cuda() )**2
+        
+        return  DO , IC
+
+
+    def calculateLoss(self , size = 2**8 , train = True):
+        
+        x , x_boundry_0 = self.sample(size)
+        x = Variable( x , requires_grad = True )
+        DO , IC  = self.criterion( x , x_boundry_0 )
+        
+        if train == True:
+            return  torch.mean( DO + IC ) , torch.mean( DO ) ,  torch.mean( IC )  
+        else:
+            return  DO , IC 
+        
+    
+    def exact_solution( self , x ):
+        return np.sin(2*np.pi*x)*np.cos(4*np.pi*x)+1
---
+++ Advection/main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 15 09:49:51 2020
+
+@author: Pooya
+
+TO DO:
+    
+"""
+
+from libs import *
+from train import *
+from net import *
+#import hook
+from advection import *
+
+#integration
+
+net = Net( NL = 2 , NN = 20 )
+net.to(torch.device("cuda:0"))  
+
+advection = Advection(net)
+#register_hook(net)
+    
+train = Train( net , advection , BATCH_SIZE = 2**8 , debug = True )
+    
+#%%
+
+train.train( epoch = 1000 , lr = 0.01 )
+
+
+#%%
+
+train.plot_report()
+train.plot_activation_mean()
+
+
+#%% PLOTS
+
+MAX_X = 1
+
+x_range = torch.tensor(np.linspace(0, MAX_X , 100, dtype=np.float)).reshape(-1,1).cuda().float()
+
+y = net(x_range).cpu().detach()
+
+plt.plot(x_range.cpu(),y)
+plt.plot(x_range.cpu(),advection.exact_solution(x_range.cpu()),'--')
+
+#%% Error - diff exact and obtained solution
+
+plt.plot( x_range.cpu() , y - advection.exact_solution(x_range.cpu()) )
